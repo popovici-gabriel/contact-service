f7c97892be8c9bc77f116f60444047909773303f
Gabriel Popovici <gabriel-sorin.popovici@ionos.com>
2019-10-29 14:27:33 +0200

    add update test

--- src/main/java/com/ionos/domains/contact/resource/ContactController.java
+++ src/main/java/com/ionos/domains/contact/resource/ContactController.java
@@ -1,18 +1,29 @@
-package com.ionos.domains.contact.create;
+package com.ionos.domains.contact.resource;
 
+import com.ionos.domains.contact.create.CreateContactService;
+import com.ionos.domains.contact.update.UpdateContactService;
 import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class CreateContactController {
+public class ContactController {
 
     @Autowired
     private CreateContactService createContactService;
+
+    @Autowired
+    private UpdateContactService updateContactService;
 
     @RequestMapping("/create")
     public String create() {
         return createContactService.createContact(UUID.randomUUID().toString());
     }
+
+    @RequestMapping("/update")
+    public String update() {
+        return updateContactService.updateContact(UUID.randomUUID().toString());
+    }
+
 }
