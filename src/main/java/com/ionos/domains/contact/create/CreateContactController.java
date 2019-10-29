ada801fcd1821ee03fec7c35d5643b4d46066849
Gabriel Popovici <gabriel-sorin.popovici@ionos.com>
2019-10-29 12:58:05 +0200

    add controller

---
+++ src/main/java/com/ionos/domains/contact/create/CreateContactController.java
@@ -0,0 +1,18 @@
+package com.ionos.domains.contact.create;
+
+import java.util.UUID;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CreateContactController {
+
+    @Autowired
+    private CreateContactService createContactService;
+
+    @RequestMapping("/create")
+    public String create() {
+        return createContactService.startCreateContact(UUID.randomUUID().toString());
+    }
+}
